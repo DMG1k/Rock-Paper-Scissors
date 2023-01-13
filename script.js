c92d91a2ff25de9d861f9792b364ea080e6d015c
DMG1k <devingalagarza1@gmail.com>
2023-01-13 17:38:50 -0500

    added the base of the rock paper scissors game. Next I need to add a 'for loop' to play the game 5 times and announce the winner after 5 games

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 function getComputerChoice() {
-    let choices = ["Rock","Paper","Scissors"];
-    let randomIndex = Math.floor(Math.random()*3);
+    const choices = ["rock","paper","scissors"];
+    const randomIndex = Math.floor(Math.random()*3);
     return choices[randomIndex];
 }
-console.log(getComputerChoice());
+
+
 
 function playRound (playerSelection, computerSelection) {
-    
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win! Rock beats Scissors";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win! Scissors beats Paper";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win! Paper beats Rock";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose! Rock beats Scissors";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose! Scissors beats Paper";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose! Paper beats Rock";
+    } else {
+        return "Invalid selection. Please choose rock, paper or scissors.";
+    }
 }
+
+let playerScore = 0;
+let compScore = 0;
+
+for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt
+    let computerSelection = getComputerChoice();
+}
+
+
