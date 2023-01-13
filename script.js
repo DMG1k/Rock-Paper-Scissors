a19e229b543562bdcfb29c43274f10728056281d
DMG1k <devingalagarza1@gmail.com>
2023-01-13 08:38:55 -0500

    created a function for the computer to randomly select rock,paper,scissors that outputs to the console

---
+++ script.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+    let choices = ["Rock","Paper","Scissors"];
+    let randomIndex = Math.floor(Math.random()*3);
+    return choices[randomIndex];
+}
+console.log(getComputerChoice());
+
+function playRound (playerSelection, computerSelection) {
+    
+}
