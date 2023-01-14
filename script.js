7bbdcf7366f078f742a22935e62f6b2274ea4f3f
DMG1k <devingalagarza1@gmail.com>
2023-01-13 19:48:56 -0500

    I got rid of the randomIndex variable and included it with the choices return

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getComputerChoice() {
     const choices = ["rock","paper","scissors"];
-    const randomIndex = Math.floor(Math.random()*3);
-    return choices[randomIndex];
+    
+    return choices[Math.floor(Math.random()*3)];
 }
 
 function playRound (playerSelection, computerSelection) {
